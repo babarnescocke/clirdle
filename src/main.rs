0a88a61a6906304d8e2dc2512290bbfd9c85a730
brian <brian@barnescocke.com>
2022-01-11 12:23:49 -0800

    returns random line from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rand::seq::IteratorRandom;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", random_line_from_file(&"../5_letter_words.txt".to_string()));
+}
+
+fn random_line_from_file(input: &String) -> String {
+let f = File::open(input)
+        .unwrap_or_else(|e| panic!("(;_;) file not found: {}: {}", input, e));
+    let f = BufReader::new(f);
+
+    let lines = f.lines().map(|l| l.expect("Couldn't read line"));
+
+    lines
+        .choose(&mut rand::thread_rng())
+        .expect("File had no lines")
 }
